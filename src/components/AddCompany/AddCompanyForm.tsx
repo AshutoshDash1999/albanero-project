be55e290e389fd1524021ee0f24444f8ff72866d
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-04-05 12:50:14 +0530

    feat: validation and publish company data to firestore

--- src/components/AddCompany/AddCompanyForm.tsx
+++ src/components/AddCompany/AddCompanyForm.tsx
@@ -1,6 +1,11 @@
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import { ChangeEvent, MouseEvent, useState } from "react";
+import { firebaseDB } from "../../utils/firebaseConfig";
 
-const AddCompanyForm = () => {
+const AddCompanyForm = (props) => {
+  const { onCloseAddCompanyModal } = props;
+
+  const [isButtonLoading, setIsButtonLoading] = useState(false);
   const [companyDetails, setCompanyDetails] = useState({
     companyName: "",
     email: "",
@@ -13,6 +18,7 @@ const AddCompanyForm = () => {
     email: false,
     phoneNumber: false,
     domain: false,
+    isFormInComplete: false,
   });
 
   const [managerList, setManagerList] = useState([
@@ -179,7 +185,45 @@ const AddCompanyForm = () => {
     );
   };
 
-  const saveCompanyDetailsHandler = () => {};
+  const saveCompanyDetailsHandler = async (e) => {
+    e.preventDefault();
+
+    managerList.forEach((manager) => {
+      if (!manager.manager) {
+        return setDetailsErrorMessage({
+          ...detailsErrorMessage,
+          isFormInComplete: true,
+        });
+      }
+
+      manager.employeeList.forEach((employee) => {
+        if (!employee.name) {
+          return setDetailsErrorMessage({
+            ...detailsErrorMessage,
+            isFormInComplete: true,
+          });
+        }
+      });
+    });
+
+    setDetailsErrorMessage({
+      ...detailsErrorMessage,
+      isFormInComplete: false,
+    });
+
+    try {
+      setIsButtonLoading(true);
+      await addDoc(collection(firebaseDB, "companyData"), {
+        ...companyDetails,
+        managerList,
+      });
+    } catch (error) {
+      console.log("Error while adding company:", error);
+    } finally {
+      setIsButtonLoading(false);
+      onCloseAddCompanyModal();
+    }
+  };
 
   return (
     <form action="" className="add__company__form">
@@ -280,7 +324,18 @@ const AddCompanyForm = () => {
         );
       })}
 
-      <button onClick={saveCompanyDetailsHandler}>Save</button>
+      {detailsErrorMessage?.isFormInComplete ? (
+        <div className="form__details__error__container">
+          <p>Please fill all details</p>
+        </div>
+      ) : null}
+
+      <div className="add__company__modal__footer">
+        <button onClick={onCloseAddCompanyModal}>Cancel</button>
+        <button onClick={saveCompanyDetailsHandler} disabled={isButtonLoading}>
+          Save
+        </button>
+      </div>
     </form>
   );
 };
